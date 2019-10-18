05d18508a9a887eaba87a347c3f838d8b3060773
Arthur Rodrigues Camine <arthur.camine@satrotas.com.br>
2019-10-18 19:23:41 -0300

    Correções cadastro

--- js/cadastro.js
+++ js/cadastro.js
@@ -4,17 +4,30 @@ function cadastrar() {
     var senha = document.getElementById("senha").value;
     var repitaSenha = document.getElementById("repita-senha").value;
 
-    if(email == '' || email.length < 5) {
-        document.getElementById("nome").style.border = "solid 1px #ff886a"; 
-        document.getElementById("pop-up-msg").style.display = "block";
+    if (nome == '' || nome.length < 5 || nome.length > 100) {
+        document.getElementById("nome").classList.add("erro");
+        document.getElementById("pop-up-nome").style.display = "block";
     }
-    else if (senha == ''){
-        document.getElementById("senha").style.border = "solid 1px #ff886a";
-        document.getElementById("pop-up-msg").style.display = "block";
+
+    if (email == '' || email.length < 7 || email.length > 100) {
+        document.getElementById("email").classList.add("erro");
+        document.getElementById("pop-up-email").style.display = "block";
+        return false;
     }
-    else {
-        document.getElementById("formulario").submit();
-    }   
+
+    if (senha == '' || senha.length < 8 || senha.length > 100) {
+        document.getElementById("senha").classList.add("erro");
+        document.getElementById("pop-up-senha").style.display = "block";
+        return false;
+    }
+
+    if (repitaSenha != senha) {
+        document.getElementById("senha").classList.add("erro");
+        document.getElementById("pop-up-repitaSenha").style.display = "block";
+        return false;
+    }
+
+    document.getElementById("formulario").submit();
 }
 
 $("#fomCadastro").onchange(function() {
